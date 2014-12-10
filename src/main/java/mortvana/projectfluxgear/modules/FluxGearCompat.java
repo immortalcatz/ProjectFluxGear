4c65677449aead9e437a0a5fa518f0f6b045265e
Mortvana <nyehcraft@gmail.com>
2014-12-10 11:38:59 -0800

    Compuswitch

---
+++ src/main/java/mortvana/projectfluxgear/modules/FluxGearCompat.java
@@ -0,0 +1,8 @@
+package mortvana.projectfluxgear.modules;
+
+public class FluxGearCompat {
+    public void preInit(){
+        boolean kroostyl, mfr, ic3, mek, tc4, m4synth, tinkers;
+    }
+
+}
--- src/main/java/mortvana/projectfluxgear/modules/imc/ThermalBuildcraft.java
+++
@@ -1,4 +0,0 @@
-package mortvana.projectfluxgear.modules.imc;
-
-public class ThermalBuildcraft {
-}
